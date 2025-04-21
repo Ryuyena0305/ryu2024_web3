7f6d6d5937d4f2873edada443aa76b431499347d
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-04-21 14:56:52 +0900

    4/21 HTTP 응답 사용하여Controller 수정

--- src/main/java/web/controller/MemberController.java
+++ src/main/java/web/controller/MemberController.java
@@ -37,7 +37,6 @@ public class MemberController {
     } // f end
 
     // [3] 로그인된 회원 검증 / 내정보 조회
-
     // @RequestHeader : HTTP 헤더 정보를 매핑 하는 어노테이션 , JWT 정보는 HTTP 헤더 에 담을 수 있다.
     // Authorization : 인증 속성 , { Authorization : token값 }
     // @RequestParam : HTTP 헤더의 경로 쿼리스트링 매핑 하는 어노테이션
